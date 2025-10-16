8965546f1bcdaa24ebf14c28d9e8fe196726a174
Alejo Ordoñez <alejoordonez02@gmail.com>
2025-10-16 13:52:02 -0300

    chore: setup pre-commit formatter

--- src/main/java/com/fiuba_groups/fiuba_groups_back/FiubaGroupsBackApplication.java
+++ src/main/java/com/fiuba_groups/fiuba_groups_back/FiubaGroupsBackApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FiubaGroupsBackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FiubaGroupsBackApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FiubaGroupsBackApplication.class, args);
+    }
 }
