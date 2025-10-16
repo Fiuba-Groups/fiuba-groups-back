d2714c84fd7158f6718f8868cb409f5e206bb663
Alejo Ordoñez <alejoordonez02@gmail.com>
2025-10-16 13:48:32 -0300

    chore: init spring boot project
    - java 21
    deps:
    - web
    - lombock
    - jpa
    - postgresql

---
+++ src/main/java/com/fiuba_groups/fiuba_groups_back/FiubaGroupsBackApplication.java
@@ -0,0 +1,13 @@
+package com.fiuba_groups.fiuba_groups_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiubaGroupsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiubaGroupsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiuba_groups/fiuba_groups_back/FiubaGroupsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiuba_groups.fiuba_groups_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiubaGroupsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
