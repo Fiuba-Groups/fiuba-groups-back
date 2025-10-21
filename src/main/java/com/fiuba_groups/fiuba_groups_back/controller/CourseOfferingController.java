017c6e3a38b4afce994fc28a2d6d84426bb41ad9
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-10-20 22:13:44 -0300

    feat(CourseOffering): controller, service & repository

---
+++ src/main/java/com/fiuba_groups/fiuba_groups_back/controller/CourseOfferingController.java
@@ -0,0 +1,23 @@
+package com.fiuba_groups.fiuba_groups_back.controller;
+
+import com.fiuba_groups.fiuba_groups_back.model.CourseOffering;
+import com.fiuba_groups.fiuba_groups_back.service.CourseOfferingService;
+import com.fiuba_groups.fiuba_groups_back.service.dto.CourseOfferingCreateRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("courseOfferings")
+public class CourseOfferingController {
+    @Autowired CourseOfferingService courseOfferingService;
+
+    @PostMapping
+    public ResponseEntity<CourseOffering> addCourseOffering(
+        @RequestBody CourseOfferingCreateRequest request) {
+        return ResponseEntity.ok(courseOfferingService.addCourseOffering(request));
+    }
+}
---
+++ src/main/java/com/fiuba_groups/fiuba_groups_back/service/CourseOfferingService.java
@@ -0,0 +1,26 @@
+package com.fiuba_groups.fiuba_groups_back.service;
+
+import com.fiuba_groups.fiuba_groups_back.exception.BadRequestException;
+import com.fiuba_groups.fiuba_groups_back.model.CourseOffering;
+import com.fiuba_groups.fiuba_groups_back.repository.CourseOfferingRepository;
+import com.fiuba_groups.fiuba_groups_back.service.dto.CourseOfferingCreateRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseOfferingService {
+    @Autowired CourseOfferingRepository courseOfferingRepository;
+
+    public CourseOffering addCourseOffering(CourseOfferingCreateRequest request) {
+        if (courseOfferingRepository.existsByQuarterAndSubjectAndCourse(
+                request.getQuarter(), request.getSubject(), request.getCourse())) {
+            throw new BadRequestException("CourseOffering already exists");
+        }
+
+        CourseOffering newCourseOffering = new CourseOffering();
+        newCourseOffering.setQuarter(request.getQuarter());
+        newCourseOffering.setSubject(request.getSubject());
+        newCourseOffering.setCourse(request.getCourse());
+        return courseOfferingRepository.save(newCourseOffering);
+    }
+}
