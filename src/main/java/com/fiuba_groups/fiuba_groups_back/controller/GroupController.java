61517faecb6e3003d7d6769bf6a1b78940fb6015
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-10-20 22:17:54 -0300

    feat(group-controller): POST groups/

--- src/main/java/com/fiuba_groups/fiuba_groups_back/controller/GroupController.java
+++ src/main/java/com/fiuba_groups/fiuba_groups_back/controller/GroupController.java
@@ -1,12 +1,19 @@
 package com.fiuba_groups.fiuba_groups_back.controller;
 
+import com.fiuba_groups.fiuba_groups_back.exception.BadRequestException;
+import com.fiuba_groups.fiuba_groups_back.exception.ResourceNotFoundException;
 import com.fiuba_groups.fiuba_groups_back.model.Group;
 import com.fiuba_groups.fiuba_groups_back.service.GroupService;
-
+import com.fiuba_groups.fiuba_groups_back.service.dto.GroupCreateRequest;
+import java.net.URI;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +24,20 @@ public class GroupController {
 
     @GetMapping("/{groupId}")
     public ResponseEntity<Group> getGroup(@PathVariable Long groupId) {
-        return groupService
-                .getGroup(groupId)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+        return groupService.getGroup(groupId)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addGroup(@RequestBody GroupCreateRequest request) {
+        try {
+            Group created = groupService.addGroup(request);
+            URI location = URI.create("/groups/" + created.getId());
+            return ResponseEntity.created(location).body(created);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("error", e.getMessage()));
+        }
     }
 }
--- src/main/java/com/fiuba_groups/fiuba_groups_back/repository/GroupRepository.java
+++ src/main/java/com/fiuba_groups/fiuba_groups_back/repository/GroupRepository.java
@@ -1,7 +1,6 @@
 package com.fiuba_groups.fiuba_groups_back.repository;
 
 import com.fiuba_groups.fiuba_groups_back.model.Group;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
