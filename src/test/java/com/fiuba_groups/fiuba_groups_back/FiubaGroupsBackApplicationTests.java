a409ea28cb306f00abb48c758e9c9eb6a68b2b96
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-10-23 10:30:54 -0300

    test(group service): successfully add a group
    + pre commit formatter

--- src/test/java/com/fiuba_groups/fiuba_groups_back/FiubaGroupsBackApplicationTests.java
+++ src/test/java/com/fiuba_groups/fiuba_groups_back/FiubaGroupsBackApplicationTests.java
@@ -1,6 +1,5 @@
 package com.fiuba_groups.fiuba_groups_back;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- src/test/java/com/fiuba_groups/fiuba_groups_back/service/GroupServiceTest.java
+++ src/test/java/com/fiuba_groups/fiuba_groups_back/service/GroupServiceTest.java
@@ -10,15 +10,15 @@ import com.fiuba_groups.fiuba_groups_back.model.CourseOffering;
 import com.fiuba_groups.fiuba_groups_back.model.Group;
 import com.fiuba_groups.fiuba_groups_back.repository.CourseOfferingRepository;
 import com.fiuba_groups.fiuba_groups_back.repository.GroupRepository;
-import com.fiuba_groups.fiuba_groups_back.service.GroupService;
 import com.fiuba_groups.fiuba_groups_back.service.dto.GroupCreateRequest;
-import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class GroupServiceTest {
@@ -35,5 +35,28 @@ public class GroupServiceTest {
         when(courseOfferingRepository.findById(1L)).thenReturn(Optional.empty());
 
         assertThrows(ResourceNotFoundException.class, () -> groupService.addGroup(req));
+    }
+
+    @Test
+    public void test01_AddGroupSuccessfullyReturnsTheAddedGroup() {
+        // setup
+        CourseOffering course = new CourseOffering();
+        course.setId(1L);
+        when(courseOfferingRepository.findById(1L)).thenReturn(Optional.of(course));
+
+        Group expected = new Group();
+        expected.setId(1L);
+        expected.setDescription("test");
+        expected.setCourseOffering(course);
+        when(groupRepository.save(any(Group.class))).thenReturn(expected);
+
+        // request
+        GroupCreateRequest req = new GroupCreateRequest();
+        req.setCourseOfferingId(1L);
+        req.setDescription("test");
+
+        Group created = groupService.addGroup(req);
+
+        assertEquals(created, expected);
     }
 }
