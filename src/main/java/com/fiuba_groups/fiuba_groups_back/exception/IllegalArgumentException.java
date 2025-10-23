1fa5bdd446a6024dd6550b4cfab3146eebd776d9
fapereyra <fpereyra@fi.uba.ar>
2025-10-23 18:50:29 -0300

    feat(exception): IllegalArgumentException

---
+++ src/main/java/com/fiuba_groups/fiuba_groups_back/exception/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package com.fiuba_groups.fiuba_groups_back.exception;
+
+public class IllegalArgumentException extends RuntimeException {
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
