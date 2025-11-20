e5057f5a6431c5d80799304a3cc3ae822acd3d6b
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-11-20 03:16:56 -0300

    refac(auth-controller): send new_user id on register (instead of email)

--- src/main/java/com/fiuba_groups/fiuba_groups_back/controller/AuthController.java
+++ src/main/java/com/fiuba_groups/fiuba_groups_back/controller/AuthController.java
@@ -5,25 +5,22 @@ import com.fiuba_groups.fiuba_groups_back.model.LoginRequest;
 import com.fiuba_groups.fiuba_groups_back.model.User;
 import com.fiuba_groups.fiuba_groups_back.security.JwtUtil;
 import com.fiuba_groups.fiuba_groups_back.service.UserService;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin(origins = "http://localhost:3000") // por ahora en local
 public class AuthController {
-
-    @Autowired
-    private UserService userService;
+    @Autowired private UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody LoginRequest request) {
         try {
             User newUser = userService.register(request.getEmail(), request.getPassword());
-            return ResponseEntity.ok(Map.of("message", "Usuario registrado con éxito", "email", newUser.getEmail()));
+            return ResponseEntity.ok(newUser.getId());
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         }
@@ -32,11 +29,12 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
         return userService.login(request.getEmail(), request.getPassword())
-                .map(user -> {
-                    String token = JwtUtil.generateToken(user.getEmail());
-                    return ResponseEntity.ok(new JwtResponse(token));
-                })
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                        .body(new JwtResponse("Credenciales inválidas")));
+            .map(user -> {
+                String token = JwtUtil.generateToken(user.getEmail());
+                return ResponseEntity.ok(new JwtResponse(token));
+            })
+            .orElseGet(()
+                           -> ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                               .body(new JwtResponse("Credenciales inválidas")));
     }
 }
