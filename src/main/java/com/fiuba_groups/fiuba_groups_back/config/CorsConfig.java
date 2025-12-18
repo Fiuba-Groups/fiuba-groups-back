5465653d321fe5c07144050bbe2fcc5be331549c
fapereyra <fpereyra@fi.uba.ar>
2025-12-18 16:50:30 -0300

    fix: copio cors de main para no pisarlo

--- src/main/java/com/fiuba_groups/fiuba_groups_back/config/CorsConfig.java
+++ src/main/java/com/fiuba_groups/fiuba_groups_back/config/CorsConfig.java
@@ -13,9 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOriginPatterns("*")
-                    .allowedMethods("*")
-                    .allowedHeaders("*");
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
             }
         };
     }
